d1933e6f1f630aef77ce5dd315a372240f533636
shishkabob27 <64564902+shishkabob27@users.noreply.github.com>
2026-08-28 03:47:41 -0500

    Update SampleConditions.cs

--- exporter/src/Events/Conditions/SampleConditions.cs
+++ exporter/src/Events/Conditions/SampleConditions.cs
@@ -33,7 +33,7 @@ public class ChannelNotPlaying : ConditionBase
 	public override int Num { get; set; } = -8;
 	public override string Build(EventBase eventBase, ref string nextLabel, ref int orIndex, Dictionary<string, object>? parameters = null, string ifStatement = "if (")
 	{
-		return $"{parameters["negatedIfStatement"]} (Application::Instance().GetBackend()->audio->SampleState({ExpressionConverter.ConvertExpression((ExpressionParameter)eventBase.Items[0].Loader, eventBase)}, true, false))) goto {nextLabel};";
+		return $"{parameters["negatedIfStatement"]} (Application::Instance().GetBackend()->audio->SampleState({ExpressionConverter.ConvertExpression((ExpressionParameter)eventBase.Items[0].Loader, eventBase)}.GetIntValue(), true, false))) goto {nextLabel};";
 	}
 }
 public class ChannelPaused : ConditionBase
